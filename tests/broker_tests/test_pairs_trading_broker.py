cf415f80019e4e9e0a109f8bc9ebf2e561d64194
Kyle Walsh <kew96@cornell.edu>
2021-11-19 10:00:10 -0500

    Adding tests

--- tests/broker_tests/test_pairs_trading_broker.py
+++ tests/broker_tests/test_pairs_trading_broker.py
@@ -13,19 +13,33 @@ class MyTestCase(unittest.TestCase):
         sim = TwoAssetSimulation(data, seed=0)
         cls.sim = sim
         cls.broker = PairsTradingBroker(
-            sim.states.iloc[0],
-            [1000, -500],
-            0.1,
-            0.2,
-            0.5,
-            1.0,
-            0.01,
-            5,
-            2
+            current_state=sim.states.iloc[0],
+            start_allocation=[1000, -500],
+            fixed_buy_cost=0.1,
+            fixed_sell_cost=0.2,
+            variable_buy_cost=0.5,
+            variable_sell_cost=1.0,
+            spread=0.01,
+            no_trade_period=5,
+            max_position=2
         )
 
     def test_slippage(self):
         self.assertEqual(self.broker.slippage, 0.005)
+
+    def test_get_trade_price(self):
+        res1 = self.broker._get_trade_price(10, True)
+        res2 = self.broker._get_trade_price(10, False)
+
+        self.assertEqual(res1, 10+0.005)
+        self.assertEqual(res2, 10-0.005)
+
+    def test_trading_costs(self):
+        res1 = self.broker._trading_costs(5, 10, 10)
+        res2 = self.broker._trading_costs(10, 5, 10)
+
+        self.assertAlmostEqual(res1, 75.1375)
+        self.assertAlmostEqual(res2, -100.15)
 
 
 if __name__ == '__main__':
