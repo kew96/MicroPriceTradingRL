70f9687103f66bb6b688406dc72186c3e8d9c5e3
Kyle Walsh <kew96@cornell.edu>
2021-11-18 23:45:08 -0500

    Adding tests for history

--- tests/history_tests/test_pairs_trading_history.py
+++ tests/history_tests/test_pairs_trading_history.py
@@ -1,6 +1,7 @@
 import unittest
 
 import numpy as np
+import pandas as pd
 
 from micro_price_trading import PairsTradingHistory, TwoAssetSimulation, Preprocess
 
@@ -59,7 +60,118 @@ class MyTestCase(unittest.TestCase):
         })
 
     def test_update_portfolio(self):
-        self.assertTrue(True)
+        current_portfolio = [-5, 10, -5]
+        shares = [1, -1]
+        state = pd.Series({'states': 10, 'mid_1': 5, 'mid_2': 10})
+
+        res1 = self.history._update_portfolio(current_portfolio, shares, state)
+
+        current_portfolio = [5, -10, 5]
+        shares = [2, -3]
+        state = pd.Series({'states': 42, 'mid_1': 2, 'mid_2': 3})
+
+        res2 = self.history._update_portfolio(current_portfolio, shares, state)
+
+        self.assertEqual(res1, [-5, 5, -10])
+        self.assertEqual(res2, [5, 4, -9])
+
+    def test_update_history(self):
+        portfolio1 = [0, 1, 2]
+        shares1 = [1, 2]
+        position1 = 1
+        steps1 = 2
+        trade_index1 = 3
+        long_short1 = True
+        period_prices1 = pd.DataFrame({'mid_1': [1, 2], 'mid_2': [3, 4]})
+
+        portfolio2 = [1, 2, 3]
+        shares2 = [2, 1]
+        position2 = 0
+        steps2 = 1
+
+        portfolio3 = [0, 1, 2]
+        shares3 = [1, 2]
+        position3 = 1
+        steps3 = 1
+
+        portfolio4 = [1, 2, 3]
+        shares4 = [2, 1]
+        position4 = 2
+        steps4 = 3
+        trade_index4 = 5
+        long_short4 = False
+        period_prices4 = pd.DataFrame({'mid_1': [1, 2, 3], 'mid_2': [3, 4, 5]})
+
+        self.history._update_history(portfolio1, shares1, position1, steps1, trade_index1, long_short1, period_prices1)
+        self.history._update_history(portfolio2, shares2, position2, steps2)
+        self.history._update_history(portfolio3, shares3, position3, steps3)
+        self.history._update_history(portfolio4, shares4, position4, steps4, trade_index4, long_short4, period_prices4)
+
+        expected_portfolio_history = [[
+                [-500, 1000, -500],
+                [0, 1, 6],  # 1
+                [0, 2, 8],  # 1
+                [1, 2, 3],  # 2
+                [0, 1, 2],  # 3
+                [1, 2, 3],  # 4
+                [1, 4, 4],  # 4
+                [1, 6, 5],  # 4
+        ]]
+        self.assertEqual(self.history.portfolio_history.tolist(), expected_portfolio_history)
+
+        expected_portfolio_values = [[
+            0,  # 0
+            7,  # 1
+            10,  # 1
+            6,  # 2
+            3,  # 3
+            6,  # 4
+            9,  # 4
+            12,  # 4
+        ]]
+        self.assertEqual(self.history.portfolio_values_history.tolist(), expected_portfolio_values)
+
+        expected_share_history = [[
+            [1000 / self.sim.states.iloc[0, 1], -500 / self.sim.states.iloc[0, 2]],  # 0
+            [1, 2],  # 1
+            [1, 2],  # 1
+            [2, 1],  # 2
+            [1, 2],  # 3
+            [2, 1],  # 4
+            [2, 1],  # 4
+            [2, 1],  # 4
+        ]]
+        self.assertEqual(self.history.share_history.tolist(), expected_share_history)
+
+        expected_positions_history = [[
+            1,  # 0
+            1,  # 1
+            1,  # 1
+            0,  # 2
+            1,  # 3
+            2,  # 4
+            2,  # 4
+            2,  # 4
+        ]]
+        self.assertEqual(self.history.positions_history.tolist(), expected_positions_history)
+
+        expected_trade_indices = [[
+            0,  # 0
+            3,  # 1
+            5,  # 4
+        ]]
+        self.assertEqual(self.history.trade_indices_history.tolist(), expected_trade_indices)
+
+        expected_long_short_indices = [[
+            0,  # 0
+            3,  # 1
+        ]]
+        self.assertEqual(self.history.long_short_indices_history.tolist(), expected_long_short_indices)
+
+        expected_short_long_indices = [[
+            5,  # 4
+        ]]
+        self.assertEqual(self.history.short_long_indices_history.tolist(), expected_short_long_indices)
 
 
 if __name__ == '__main__':
