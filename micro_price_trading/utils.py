6538b88750167973fcd6244b144b3e0dae81a65c
Kyle Walsh <kew96@cornell.edu>
2021-11-27 01:46:36 -0500

    Updating overrage penalty

--- micro_price_trading/utils.py
+++ micro_price_trading/utils.py
@@ -5,12 +5,12 @@ from micro_price_trading.history.optimal_execution_history import Portfolio
 
 def first_price_reward(current_portfolio: Portfolio, prices_at_start: np.ndarray, target_risk: int):
     diff = 0
-    if target_risk > current_portfolio.total_risk:
-        return -(target_risk - current_portfolio.total_risk)**2
     if current_portfolio.trade:
         diff += (prices_at_start[current_portfolio.trade.asset - 1] * current_portfolio.trade.shares
                  - current_portfolio.trade.cost)
     if current_portfolio.penalty_trade:
         diff += (prices_at_start[current_portfolio.penalty_trade.asset - 1] * current_portfolio.penalty_trade.shares
                  - current_portfolio.penalty_trade.cost)
-    return diff
+    if target_risk < current_portfolio.total_risk:
+        return -abs(diff) * (current_portfolio.total_risk - target_risk), 'risk penalty'
+    return diff, 'actual'
