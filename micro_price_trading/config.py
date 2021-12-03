ebb1c91e8a5cd20cda8f94665461edea7afaf18e
Kyle Walsh <kew96@cornell.edu>
2021-12-03 16:25:17 -0500

    Add path

--- micro_price_trading/config.py
+++ micro_price_trading/config.py
@@ -4,7 +4,8 @@ from pathlib import Path
 __CONFIG_PATH = Path(__file__)
 DATA_PATH = __CONFIG_PATH.parent.parent.joinpath('asset_data')
 
-OPTIMAL_EXECUTION_FIGURES = __CONFIG_PATH.parent.parent.joinpath('OptimalExecutionRL', 'figures')
+OPTIMAL_EXECUTION_RL = __CONFIG_PATH.parent.parent.joinpath('OptimalExecutionRL')
+OPTIMAL_EXECUTION_FIGURES = OPTIMAL_EXECUTION_RL.joinpath('figures')
 PAIRS_TRADING_FIGURES = __CONFIG_PATH.parent.parent.joinpath('PairsTradingRL', 'figures')
 
 TEN_SECOND_DAY = 2340
