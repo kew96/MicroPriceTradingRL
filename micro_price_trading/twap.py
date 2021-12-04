14b71b5deacc6644c6b523c6a9665710118ae81d
Kyle Walsh <kew96@cornell.edu>
2021-12-03 22:51:39 -0500

    Multiprocessing

--- micro_price_trading/twap.py
+++ micro_price_trading/twap.py
@@ -1,6 +1,8 @@
+from functools import partial
+from multiprocessing import cpu_count, Manager, Process
+
 import numpy as np
 import pandas as pd
-from tqdm.auto import tqdm
 
 from micro_price_trading.config import DATA_PATH, OPTIMAL_EXECUTION_RL, TWENTY_SECOND_DAY
 
@@ -61,41 +63,75 @@ class TWAP:
 
         return np.array(avg_prices_1), np.array(avg_prices_2)
 
-    def continuous_twap(self, threshold=0.15):
+    def continuous_twap(self, threshold=0.15, verbose=False):
+        def _starmap_continuous_twap(data, inner_threshold, asset1_buy_prices, asset2_buy_prices):
+            intervals = list()
 
-        intervals = [
-            range(idx * self.trade_interval, (idx + 1) * self.trade_interval)
-            for idx in range(self.data.shape[1] // self.trade_interval)
-        ]
-        asset1_buy_prices = list()
-        asset2_buy_prices = list()
-        data_copy = self.data[:, :, :].copy()
+            for idx in range(self.data.shape[1]//self.trade_interval+1):
+                new_interval = range(idx*self.trade_interval, min((idx+1)*self.trade_interval, self.steps_in_day))
+                if new_interval:
+                    intervals.append(new_interval)
 
-        for sim in tqdm(data_copy, desc=f'Threshold = {threshold}'):
-            a1_buys = list()
-            a2_buys = list()
-            for interval in intervals:
-                subset = sim[interval, :]
-                for idx, entry in enumerate(subset):
-                    q_vals = self.q_values.iloc[int(entry[0])].copy()
-                    choice = np.argmax(q_vals)
-                    if choice == 0 and q_vals[0] - q_vals[1] > threshold:
-                        a1_buys.extend([True] + [False] * (self.trade_interval - idx - 1))
-                        a2_buys.extend([False] * (self.trade_interval - idx))
-                        break
-                    elif choice == 1 and q_vals[1] - q_vals[0] > threshold:
-                        a1_buys.extend([False] * (self.trade_interval - idx))
-                        a2_buys.extend([True] + [False] * (self.trade_interval - idx - 1))
-                        break
-                    else:
-                        a1_buys.append(False)
-                        a2_buys.append(False)
-                if a1_buys[-1] + a2_buys[-1] < 1:
-                    choice = np.argmax(self.q_values.iloc[int(entry[0]), 1:])
-                    if choice == 0:
-                        a1_buys[-1] = True
-                    else:
-                        a2_buys[-1] = True
-            asset1_buy_prices.append(sim[a1_buys, 1])
-            asset2_buy_prices.append(sim[a2_buys, 2])
-        return np.array(asset1_buy_prices, dtype=object), np.array(asset2_buy_prices, dtype=object)
+            for sim in data[:, :-1, :]:
+                a1_buys = list()
+                a2_buys = list()
+                for interval in intervals:
+                    subset = sim[interval, :]
+                    for idx, entry in enumerate(subset):
+                        q_vals = self.q_values.iloc[int(entry[0])].copy()
+                        choice = np.argmax(q_vals)
+                        if choice == 0 and q_vals[0]-q_vals[1] > inner_threshold:
+                            a1_buys.extend([True]+[False]*(self.trade_interval - idx-1))
+                            a2_buys.extend([False]*(self.trade_interval - idx))
+                            break
+                        elif choice == 1 and q_vals[1]-q_vals[0] > inner_threshold:
+                            a1_buys.extend([False]*(self.trade_interval - idx))
+                            a2_buys.extend([True]+[False]*(self.trade_interval - idx-1))
+                            break
+                        else:
+                            a1_buys.append(False)
+                            a2_buys.append(False)
+                    if a1_buys[-1] + a2_buys[-1] < 1:
+                        choice = np.argmax(self.q_values.iloc[int(entry[0]), 1:])
+                        if choice == 0:
+                            a1_buys[-1] = True
+                        else:
+                            a2_buys[-1] = True
+
+                asset1_buy_prices.append(sim[a1_buys[:len(sim)], 1])
+                asset2_buy_prices.append(sim[a2_buys[:len(sim)], 2])
+
+        num_processors = min(cpu_count() - 1, 12)
+
+        data_splits = [arr.copy() for arr in np.array_split(self.data, num_processors)]
+        thresholds = [threshold] * num_processors
+
+        manager = Manager()
+        asset1_prices = manager.list()
+        asset2_prices = manager.list()
+
+        part_optimal = partial(
+            _starmap_continuous_twap,
+            asset1_buy_prices=asset1_prices,
+            asset2_buy_prices=asset2_prices
+        )
+
+        args = zip(data_splits, thresholds)
+
+        all_processes = list()
+
+        for arg in args:
+            p = Process(target=part_optimal, args=arg)
+            all_processes.append(p)
+            p.start()
+
+        for p in all_processes:
+            p.join()
+
+        asset1_prices = np.array(asset1_prices, dtype=object)
+        asset2_prices = np.array(asset2_prices, dtype=object)
+
+        if verbose:
+            print(f'Done: Threshold = {threshold}, Trade Interval = {self.trade_interval}')
+
+        return asset1_prices, asset2_prices
