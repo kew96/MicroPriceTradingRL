c4f0e46a6a929a2a560a7c055b841a5bd43fda07
Kyle Walsh <kew96@cornell.edu>
2021-12-04 00:23:05 -0500

    Fix calculations for nan

--- micro_price_trading/utils.py
+++ micro_price_trading/utils.py
@@ -4,9 +4,9 @@ from scipy import stats
 
 def CI(array, confidence=.95):
     if type(array[0]) not in {int, float}:
-        array = [np.mean(arr) for arr in array]
+        array = [np.nanmean(arr) for arr in array]
     cdf_value = confidence + (1 - confidence) / 2
     z = stats.norm.ppf(cdf_value)
-    half_width = z * np.std(array) / np.sqrt(len(array))
-    mu = np.mean(array)
+    half_width = z * np.nanstd(array) / np.sqrt((~np.isnan(array)).sum())
+    mu = np.nanmean(array)
     return mu - half_width, mu, mu + half_width
