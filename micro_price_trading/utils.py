21348e6f9ef7b1421c7fe7ad795fc19a2fccd802
Kyle Walsh <kew96@cornell.edu>
2021-12-05 13:56:49 -0500

    Add function for comparing strategies

--- micro_price_trading/utils.py
+++ micro_price_trading/utils.py
@@ -6,13 +6,36 @@ from micro_price_trading.config import DATA_PATH
 
 
 def CI(array, confidence=.95):
+    if len(np.array(array).shape) > 1 and np.array(array).shape[0] == 1:
+        array = array[0]
     if type(array[0]) not in {int, float}:
-        array = [np.nanmean(arr) for arr in array]
+        array = [pd.Series(arr).mean() for arr in array]
     cdf_value = confidence + (1 - confidence) / 2
     z = stats.norm.ppf(cdf_value)
-    half_width = z * np.nanstd(array) / np.sqrt((~np.isnan(array)).sum())
-    mu = np.nanmean(array)
+    half_width = np.round(z * pd.Series(array).std() / np.sqrt((~pd.Series(array).isna()).sum()), 4)
+    mu = np.round(pd.Series(array).mean(), 4)
     return mu - half_width, mu, mu + half_width
+
+
+def compare_executions(baseline, results, buy=True, spread=0.01, confidence=0.95):
+    if len(np.array(baseline).shape) > 1 and np.array(baseline).shape[0] == 1:
+        baseline = baseline[0]
+
+    if len(np.array(results).shape) > 1 and np.array(results).shape[0] == 1:
+        results = results[0]
+
+    if type(baseline[0]) not in {int, float}:
+        baseline = np.array([pd.Series(arr).mean() for arr in baseline])
+
+    if type(results[0]) not in {int, float}:
+        results = np.array([pd.Series(arr).mean() for arr in results])
+
+    if buy:
+        comps = (baseline - results) / spread * 100
+    else:
+        comps = (results - baseline) / spread * 100
+
+    return CI(comps, confidence=confidence)
 
 
 def save_q_values(env, algo, file_name, columns=None):
