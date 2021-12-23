c8a9b65d4a6b96a62e1a7bb73ece1c810f3668eb
Kyle Walsh <kew96@cornell.edu>
2021-12-23 18:24:48 -0500

    RLMM-3 Working on history

--- micro_price_trading/history/pairs_trading_history.py
+++ micro_price_trading/history/pairs_trading_history.py
@@ -14,23 +14,23 @@ class PairsTradingHistory(History):
     steps when needed. Generally, a set of arrays and a few functions all related to data storage.
 
     Attributes:
-        portfolio: The current portfolio (cash, asset 1, asset 2) in dollars
-        portfolio_value: The current portfolio value
-        shares: The current shares held of asset 1, asset 2
-        position: The current leverage position
-        max_position: The maximum amount of `leverages` allowed, i.e. 5 means you can be 5x Long/Short or 5x
-            Short/Long at any time, at most
-        num_trades: The number of trades of each type as a list of dictionaries
+        current_portfolio: The current PairsTradingPortfolio
         readable_action_space: The human readable format of the actions
 
     Properties:
-        portfolio_history: An array of the dollar positions (cash, asset 1, asset 2) for each step in all runs
-        portfolio_values_history: An array of the dollar value of the portfolio for each step in all runs
-        share_history: An array of amount of shares of each asset for each step in all runs
+        portfolio_history: An array of the PairsTradingPortfolios from all iterations
+        share_history: An array of the amount of shares of each asset for each step in all runs
+        cash_history: An array of the cash position for each step in all runs
+        asset_paths: An array of the asset prices for each step in all runs
         positions_history: The amount of leverage for each step in all runs
-        trade_indices_history: An array of all trade indices for each step in all runs
-        long_short_indices_history: An array of all long/short trade indices for each step in all runs
-        short_long_indices_history: An array of all short/long trade indices for each step in all runs
+        portfolio_value_history: An array of the dollar value of the portfolio for each step in all runs
+
+    Methods:
+        _generate_readable_action_space: Creates a dictionary mapping actions to their human readable format
+        _update_history: Stores current portfolio and any additional ones if prices are passed
+        _collapse_state_trades: Creates a dictionary with residual imbalance states and positions as keys and the
+            corresponding number of trades as the values
+        _
     """
 
     def __init__(
@@ -69,13 +69,11 @@ class PairsTradingHistory(History):
             res_imbalance_state=reverse_mapping.get(start_state[0], '---'),
         )
 
-        self._portfolios = [[self.current_portfolio]]
-
-        # dict: keys are states, values are lists of actions taken in that state
-        self.num_trades = [dict()]
+        self._portfolios = [[]]
 
         # USED FOR RESET ONLY
         self._start_allocation = start_allocation
+        self._start_cash = start_cash
 
         # NEEDED FOR UPDATING DICTIONARY
         self.__reverse_mapping = reverse_mapping
@@ -91,14 +89,6 @@ class PairsTradingHistory(History):
 
         get_shares = np.vectorize(get_shares)
         return np.dstack(get_shares(self.portfolio_history))
-
-    @property
-    def risk_history(self) -> np.ndarray:
-        def get_risk(portfolio):
-            return portfolio.total_risk
-
-        get_risk = np.vectorize(get_risk)
-        return get_risk(self.portfolio_history)
 
     @property
     def cash_history(self) -> np.array:
@@ -132,6 +122,30 @@ class PairsTradingHistory(History):
         get_values = np.vectorize(get_value)
         return np.array(get_values(self.portfolio_history))
 
+    @staticmethod
+    def _generate_readable_action_space(max_position):
+        """
+        Creates a dictionary from integer actions to human readable positions
+
+        Args:
+            max_position: The maximum amount of `leverages` allowed, i.e. 5 means you can be 5x Long/Short or 5x
+                Short/Long at any time, at most
+
+        Returns: A dictionary mapping of int -> string of integer actions to string representations
+
+        """
+        actions = dict()
+        n_actions = max_position * 2 + 1
+
+        for key in range(n_actions):
+            if key < n_actions // 2:
+                actions[key] = f'Short/Long {n_actions // 2 - key}x'
+            elif key > n_actions // 2:
+                actions[key] = f'Long/Short {key - n_actions // 2}x'
+            else:
+                actions[key] = 'Flat'
+        return actions
+
     def _update_history(
             self,
             portfolio: PairsTradingPortfolio,
@@ -150,60 +164,40 @@ class PairsTradingHistory(History):
             period_states: A Pandas DataFrame with the first and second columns as the asset prices,
                 `len(period_prices)` should equal `steps`. Should include the current prices as the first row.
         """
+        portfolios = [portfolio]
         if period_states:
-            portfolios = [portfolio]
             for state in period_states:
                 portfolio = portfolio.copy_portfolio(
                     self.__reverse_mapping.get(state[0], '---'),
                     state[1:]
                 )
-        else:
-            self._portfolio_history[-1].append(portfolio)
+                portfolios.append(portfolio)
+        self._portfolios.extend(portfolios)
 
-
-    def _collapse_num_trades_dict(
+    def _collapse_state_trades(
             self,
             num_env_to_analyze: int = 1
     ):
-        """
-        This combines the last num_env_to_analyze dictionaries in self.num_trades into one dictionary
-        Every time env.reset() gets called, a new entry in self.num_trades is appended
-        :param num_env_to_analyze: integer representing number of dictionaries in self.num_trades to be combined
-        :return:
-        """
-        collapsed = self.num_trades[-num_env_to_analyze]
-        for i in range(len(self.num_trades) - num_env_to_analyze + 1, len(self.num_trades)):
-            for k, v in self.num_trades[i].items():
-                current = collapsed.get(k, []) + v
-                collapsed[k] = current
-        return collapsed
+        num_trades = dict()
 
-    def _update_num_trades(
-            self,
-            action: int,
-            current_state: pd.Series
-    ):
-        reverse_mapped_state = self.__reverse_mapping[current_state[0]]
-        num_trades_last = self.num_trades[-1].get(reverse_mapped_state, []) + [action]
-        self.num_trades[-1][reverse_mapped_state] = num_trades_last
+        for portfolio_set in self.portfolio_history[-num_env_to_analyze:]:
+            for portfolio in portfolio_set:
+                if portfolio.trade:
+                    num_trades[
+                        (portfolio.res_imbalance_state, portfolio.trade.shares*np.sign(portfolio.trade.total_cost))
+                    ] = num_trades.get(
+                        (portfolio.res_imbalance_state, portfolio.trade.shares*np.sign(portfolio.trade.total_cost)),
+                        0
+                    ) + 1
 
-    def _reset_history(self, current_state):
-        self.portfolio = [-sum(self._start_allocation), *self._start_allocation]
-        self._portfolio_history.append([self.portfolio])
+        return num_trades
 
-        self.portfolio_value = sum(self.portfolio)
-        self._portfolio_values_history.append([self.portfolio_value])
-
-        self.shares = [self._start_allocation[0] / current_state[1], self._start_allocation[1] / current_state[2]]
-        self._share_history.append([self.shares])
-
-        self.position = 1
-        self._positions_history.append([1])
-
-        self._trade_indices_history.append([0])
-
-        self._long_short_indices_history.append([0])
-
-        self._short_long_indices_history.append([])
-
-        self.num_trades.append(dict())
+    def _reset_history(self, start_state):
+        self.current_portfolio = PairsTradingPortfolio(
+            time=0,
+            cash=self._start_cash,
+            shares=self._start_allocation,
+            mid_prices=tuple(start_state[1:]),
+            res_imbalance_state=self.__reverse_mapping.get(start_state[0], '---'),
+        )
+        self._portfolios.append([])
