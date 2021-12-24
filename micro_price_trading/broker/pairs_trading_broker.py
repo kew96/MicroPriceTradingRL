6a4f1e08f9f0fa13e50d8c2cc8d891010a3f31a7
Kyle Walsh <kew96@cornell.edu>
2021-12-23 22:43:14 -0500

    RLMM-14 Remove function definitions and unused parameters

--- micro_price_trading/broker/pairs_trading_broker.py
+++ micro_price_trading/broker/pairs_trading_broker.py
@@ -1,12 +1,11 @@
-from typing import List, Union, Optional
+from typing import List, Union
 
 import pandas as pd
 
 from .broker import Broker
-from micro_price_trading.history.pairs_trading_history import PairsTradingHistory, Allocation
 
 
-class PairsTradingBroker(Broker, PairsTradingHistory):
+class PairsTradingBroker(Broker):
 
     """
     The main class to deal with trading in our specific pairs trading environment. Generally takes care of the actual
@@ -38,16 +37,13 @@ class PairsTradingBroker(Broker, PairsTradingHistory):
 
     def __init__(
             self,
-            current_state: pd.Series,
-            start_allocation: Allocation = None,
             fixed_buy_cost: float = 0.0,
             fixed_sell_cost: float = 0.0,
             variable_buy_cost: float = 0.0,
             variable_sell_cost: float = 0.0,
             spread: Union[float, int] = 0,
             no_trade_period: int = 0,
-            max_position: int = 10,
-            reverse_mapping: Optional[dict] = None
+            max_position: int = 10
     ):
         """
 
@@ -65,13 +61,6 @@ class PairsTradingBroker(Broker, PairsTradingHistory):
             max_position: The maximum amount of `leverages` allowed, i.e. 5 means you can be 5x Long/Short or 5x
                 Short/Long at any time, at most
         """
-        PairsTradingHistory.__init__(
-            self,
-            current_state=current_state,
-            start_allocation=start_allocation,
-            reverse_mapping=reverse_mapping,
-            max_position=max_position
-        )
 
         # Trading costs
         self.fixed_buy_cost = fixed_buy_cost
@@ -113,43 +102,7 @@ class PairsTradingBroker(Broker, PairsTradingHistory):
             and the second being the number of shares in each asset
 
         """
-        current_shares_asset1 = current_portfolio[1] / current_state[1]
-        current_shares_asset2 = current_portfolio[2] / current_state[2]
-
-        target_asset1 = action * dollar_amount[0]
-        target_asset2 = action * dollar_amount[1]
-
-        target_shares_asset1 = target_asset1 / current_state[1]
-        target_shares_asset2 = target_asset2 / current_state[2]
-
-        cost_asset1 = self._trading_costs(
-            current_shares=current_shares_asset1,
-            target_shares=target_shares_asset1,
-            mid_price=current_state[1]
-        )
-
-        cost_asset2 = self._trading_costs(
-            current_shares=current_shares_asset2,
-            target_shares=target_shares_asset2,
-            mid_price=current_state[2]
-        )
-
-        trading_cost = cost_asset1 + cost_asset2
-
-        new_cash = current_portfolio[0] - trading_cost
-
-        new_portfolio = [
-            new_cash,
-            target_asset1,
-            target_asset2
-        ]
-
-        new_shares = [
-            target_shares_asset1,
-            target_shares_asset2
-        ]
-
-        return new_portfolio, new_shares
+        ...
 
     def _trading_costs(self, current_shares, target_shares, mid_price):
         """
@@ -165,19 +118,7 @@ class PairsTradingBroker(Broker, PairsTradingHistory):
             bid/ask spread
 
         """
-        buy = target_shares > current_shares
-        price = self._get_trade_price(mid_price, buy)
-        quantity = target_shares - current_shares
-
-        if buy:
-            costs = quantity * price * (1 + self.variable_buy_cost)
-            costs += self.fixed_buy_cost
-
-        else:
-            costs = quantity * price * (1 + self.variable_sell_cost)
-            costs += self.fixed_sell_cost
-
-        return costs
+        ...
 
     def _get_trade_price(self, mid_price, buy):
         """
@@ -190,13 +131,8 @@ class PairsTradingBroker(Broker, PairsTradingHistory):
         Returns: A float of the trade price
 
         """
-        trade_price = mid_price + (buy * 2 - 1) * self.slippage
-        return trade_price
+        ...
 
     def _reset_broker(self, current_state):
-        PairsTradingHistory._reset_history(
-            self,
-            current_state=current_state
-        )
 
         self._traded = False
